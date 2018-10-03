497b706644ecab9dfbefa4c2197e2e5205016fee
Ran Bi <35747244+iamranbi@users.noreply.github.com>
2018-10-03 03:47:38 -0400

    Update tweepy2.py

--- tweepy2.py
+++ tweepy2.py
@@ -5,15 +5,14 @@
 import sqlite3
 
 DB_NAME='tweets.sqlite'
-
 try:
     conn=sqlite3.connect(DB_NAME)
     cur=conn.cursor()
 except Error as e:
     print(e)
 
+# print the tweet with the most number of retweets
 def get_most_retweeted_tweet():
-    #prints the tweet with the most number of retweets (just tweet text will do)
     st_most_retweeted="SELECT TweetText FROM Tweets ORDER BY RetweetCount DESC LIMIT 1"
     cur.execute(st_most_retweeted)
     print("tweet with the most number of retweets: \n")
@@ -21,8 +20,8 @@ def get_most_retweeted_tweet():
         print(row[0].decode('utf8'))
     print("------------------------------------------------------------")
 
+# print the user’s screen name who is most followed
 def get_most_followed_user():
-    #print the user’s screen name who is most followed
     st_most_followed="SELECT ScreenName FROM Tweets ORDER BY FollowerCount DESC LIMIT 1"
     cur.execute(st_most_followed)
     print("user’s screen name who is most followed: \n")
@@ -30,8 +29,8 @@ def get_most_followed_user():
         print(row[0])
     print("------------------------------------------------------------")
 
+# print the user’s screen name who’s tweet had the highest retweet count
 def get_most_retweeted_user():
-    #print the user’s screen name who’s tweet had the highest retweet count
     st_retweeted_user="SELECT ScreenName FROM Tweets ORDER BY RetweetCount DESC LIMIT 1"
     cur.execute(st_retweeted_user)
     print("user’s screen name whose tweet had the highest retweet count: \n")
@@ -39,8 +38,8 @@ def get_most_retweeted_user():
         print(row[0])
     print("------------------------------------------------------------")
 
+# print the 5 tweets that belong to authors with highest number of followers (in descending order)
 def get_tweets_from_most_followed():
-    #print the 5 tweets that belong to authors with highest number of followers. Order this in the the descending order.
     st_tw_followed="SELECT TweetText FROM Tweets ORDER BY FollowerCount DESC LIMIT 5"
     cur.execute(st_tw_followed)
     print("5 tweets that belong to authors with highest number of followers: \n")
@@ -48,12 +47,12 @@ def get_tweets_from_most_followed():
         print(row[0].decode('utf8'))
     print("------------------------------------------------------------")
 
+# print the top 5 locations that are tweeting
 def get_trending_location():
-    #print the top 5 locations that are tweeting about us/ Going Blue. Order in the descending order.
     st_location="SELECT Location FROM Tweets GROUP BY Location "
     st_location+="HAVING Location !='' ORDER BY COUNT(*) DESC LIMIT 5"
     cur.execute(st_location)
-    print("top 5 locations that are tweeting about Go Blue: \n")
+    print("top 5 locations that are tweeting: \n")
     for row in cur:
         print(row[0])
     print("------------------------------------------------------------")
