e48786ed90289a34bb06cf0b976e46ead8873c02
Ran Bi <35747244+iamranbi@users.noreply.github.com>
2018-10-03 01:48:04 -0400

    Update northwind.py

--- northwind.py
+++ northwind.py
@@ -1,4 +1,5 @@
 import sqlite3 as sqlite
+from datetime import datetime
 conn=sqlite.connect('Northwind_small.sqlite')
 cur=conn.cursor()
 
@@ -54,5 +55,34 @@ print('Show all columns in the Order table: \n')
 cur.execute('SELECT * FROM [Order]')
 for i in cur.description:
     print(i[0])
-## column info: for x in cursor.description: print(x)
 print('-'*20)
+
+# employee's first name and the number of order each employee has made (sort by the total number of orders)
+print('Identify first name and number of order has made of each employee: \n')
+st1='SELECT Employee.FirstName, COUNT(*) FROM Employee JOIN [Order] '
+st1+='ON Employee.Id=[Order].EmployeeId GROUP BY FirstName ORDER BY COUNT(*) DESC'
+cur.execute(st1)
+for row in cur:
+    print(row[0]+', '+str(row[1]))
+print('-'*20)
+
+# products and the corresponding supply companies in Ann Arbor
+print('Identify the products and the corresponding supply companies in Ann Arbor: \n')
+st2='SELECT Product.ProductName, Supplier.CompanyName FROM Product JOIN Supplier '
+st2+='ON Product.SupplierId=Supplier.Id WHERE Supplier.City=="Ann Arbor"'
+cur.execute(st2)
+for row in cur:
+    print(row[0]+', '+str(row[1]))
+print('-'*20)
+
+# number of days passed between orders for each customer(self join)
+date_format = "%Y-%m-%d"
+st3='SELECT x.CustomerId, y.OrderDate AS OrderDate, x.OrderDate AS PreviousOrderDate '
+st3=st3+'FROM [Order] AS x JOIN [Order] AS y On x.CustomerId=y.CustomerId '
+st3=st3+'WHERE y.OrderDate>x.OrderDate GROUP BY x.CustomerId, y.OrderDate'
+cur.execute(st3)
+
+print('Customer ID, Order Date, Previous Order Date, Days Passed')
+for row in cur:
+    da=datetime.strptime(row[1],date_format)-datetime.strptime(row[2],date_format)
+    print(row[0]+', '+row[1]+', '+row[2]+', '+str(da.days))
